10746e2072d518ad85febd8e51d86b4b477304c4
Robert Castillo <robertstevencastillo@gmail.com>
2020-07-10 18:29:06 -0400

    Fixing PostCss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,14 +2,14 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx", "./src/**/*.js"],
-    ...(process.env.NODE_ENV === "production"
-      ? {
-          "@fullhuman/postcss-purgecss": {
-            content: ["./components/**/*.js", "./pages/**/*.js"],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        }
-      : {}),
   },
+  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx", "./src/**/*.js"],
+  ...(process.env.NODE_ENV === "production"
+    ? {
+        "@fullhuman/postcss-purgecss": {
+          content: ["./components/**/*.js", "./pages/**/*.js"],
+          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        },
+      }
+    : {}),
 };
