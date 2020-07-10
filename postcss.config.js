881f9b3c78a04ae51589a880184f42e225c198f7
Robert Castillo <robertstevencastillo@gmail.com>
2020-07-10 18:25:55 -0400

    Fixing PostCss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,7 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    purge: {
-      // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-      enabled: process.env.NODE_ENV === "production",
-      content: ["components/**/*.vue", "layouts/**/*.vue", "pages/**/*.vue", "plugins/**/*.js", "nuxt.config.js"],
-    },
+    purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx", "./src/**/*.js"],
     ...(process.env.NODE_ENV === "production"
       ? {
           "@fullhuman/postcss-purgecss": {
